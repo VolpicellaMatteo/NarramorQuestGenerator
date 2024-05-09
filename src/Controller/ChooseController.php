d56f20268803a23af90b5ba94866da15f68ee2c7
Volpicella Matteo <100648661+VolpicellaMatteo@users.noreply.github.com>
2024-05-09 09:50:33 +0200

    database setup

--- src/Controller/ChooseController.php
+++ src/Controller/ChooseController.php
@@ -11,8 +11,15 @@ class ChooseController extends AbstractController
     #[Route('/choose', name: 'choose')]
     public function index(): Response
     {
+
+        
+
         return $this->render('main/choose.html.twig', [
             'controller_name' => 'ChooseController',
+            'player' => $_POST['player'],
+            'place' => $_POST['place'],
+            'npc' => $_POST['npc'],
+            'quest_type' => $_POST['quest_type'],
         ]);
     }
 }
---
+++ src/Model/Quest/CurrentQuest.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Model\Quest;
+
+class CurrentQuest
+{
+    
+}
